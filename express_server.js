aec40f612034a3d413083c027dd61dcd034644b3
gavinswan <gavtswan@gmail.com>
2021-02-16 22:33:52 +0000

    Added some comments

--- express_server.js
+++ express_server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 8080; //default port
 const bodyParser = require("body-parser");
 const generateRandomString = function () {
   return Math.random().toString(36).substring(2, 8);
 }
 
 app.use(bodyParser.urlencoded({extended: true}));
+//set the view engine to ejs
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -14,14 +15,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//route path
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/hello", (req, res) => {
+//showing how html can be included in .js but not ideal
+app.get("/hello", (req, res) => { 
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//res.render sends a view to the user...looks in the views dir for the views
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -31,8 +35,10 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//add another page to display a single url and it's shortened form
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const shortURL = req.params.shortURL;
+  const templateVars = { shortURL, longURL: urlDatabase[shortURL]};
   res.render("urls_show", templateVars);
 });
 
@@ -50,7 +56,6 @@ app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   const longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
-  console.log(req.body);
   res.redirect(`/urls/${shortURL}`);
 });
 
