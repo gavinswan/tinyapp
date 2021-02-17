d22385f0f6c316a061be5fc2628409f7d50ef457
gavinswan <gavtswan@gmail.com>
2021-02-17 17:16:49 +0000

    Added logout/login control, and cookie clearer

--- express_server.js
+++ express_server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const generateRandomString = function () {
   return Math.random().toString(36).substring(2, 8);
 }
+
+app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({extended: true}));
 //set the view engine to ejs
@@ -27,30 +30,40 @@ app.get("/hello", (req, res) => {
 
 //res.render sends a view to the user...looks in the views dir for the views
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+  console.log(templateVars);
+  console.log(req.cookies);
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 //add another page to display a single url and it's shortened form
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  const templateVars = { shortURL, longURL: urlDatabase[shortURL]};
+  const templateVars = {
+    shortURL, longURL: urlDatabase[shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
 
 app.get("/urls/:id", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-const longURL = urlDatabase[req.params.shortURL];
-const formattedUrl = longURL.includes("http") ? longURL : `http://${longURL}`;
-res.redirect(formattedUrl);
+  const longURL = urlDatabase[req.params.shortURL];
+  const formattedUrl = longURL.includes("http") ? longURL : `http://${longURL}`;
+  res.redirect(formattedUrl);
 });
 
 // create new url
@@ -75,6 +88,18 @@ app.post("/urls/:shortURL", (req, res) => {
     urlDatabase[shortURL] = longURL;
     res.redirect(`/urls/${shortURL}`);
   });
+
+// handle a post to /login - set a cookie named username - redirect back to /urls
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
 
   app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
